f29fdbeb29d7cf142edcfa985e49c472576aaae6
ArlenWang <wangarlen@gmail.com>
2019-12-04 18:05:52 +0800

    edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import './assets/css/Index.css';
 //import { createBrowserHistory } from "history";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import indexRoutes from "./router/index";
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import AppReducer from './reducers';
+import rootSaga from './sagas';
+import createSagaMiddleware from 'redux-saga';
+const sagaMiddleware = createSagaMiddleware();
 //const hist = createBrowserHistory();
+const store = createStore(
+  AppReducer
+  , applyMiddleware(sagaMiddleware),
+);
+sagaMiddleware.run(rootSaga);
 function App() {
   return (
-    <Router>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} component={prop.component} key={key} />;
-      })}
-    </Switch>
-  </Router>
+    <Provider store={store}>
+
+      <Router>
+        <Switch>
+          {indexRoutes.map((prop, key) => {
+            return <Route path={prop.path} component={prop.component} key={key} />;
+          })}
+        </Switch>
+      </Router>
+
+    </Provider>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/css/Index.css';
+//import ReactDOM from 'react-dom';
 import App from './asyncLoading/AppLoading';
 import * as serviceWorker from './serviceWorker';
-
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import AppReducer from './reducers';
-import rootSaga from './sagas';
-import createSagaMiddleware from 'redux-saga';
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-    AppReducer
-   ,applyMiddleware(sagaMiddleware),
-  );
-sagaMiddleware.run(rootSaga);
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
-
-serviceWorker.unregister();
+import("react-dom").then(ReactDOM => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  serviceWorker.unregister();
+});
